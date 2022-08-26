5104777e7cf6683243217325e11d468e2dad0409
Luca Ghislotti <ghislottiluca@gmail.com>
2022-08-26 16:16:55 +0200

    Update elaborate_scan.m

--- elaborate_scan.m
+++ elaborate_scan.m
@@ -11,7 +11,52 @@ for i = 1:length(index_interpreter)
 end
 
 
-%% SINGLE CHANNELS
+%% SINGLE CHANNELS (THR = 200)
+
+importedData = readmatrix(['input/scan_carica_TH_200.dat']);
+
+ENC = nan(31, 1);
+THR = nan(31, 1);
+
+for ch = 0:31
+    data = importedData(importedData(:,5)==ch,1:5);
+    data = data(data(:,2) < 200,:);
+    f = figure("Visible", "off");
+    box on
+    grid on
+    hold on
+    X = data(:,2)*0.841;
+    DATA =  data(:,4)/10;
+    plot(X, DATA);
+    [f, x, flo, fup] = ecdf(data(:,4)/10, 'Bounds','on');
+    hold off
+    val_min = max(X(DATA == 0))
+    if max(DATA) >= 98 & min(DATA) == 0
+        val_max = min(X(DATA >= 98))
+        std = val_max - val_min
+        THR(ch+1, 1) = val_min + std/2;
+        ENC(ch+1, 1) = std/2 * 2.35;
+    else
+        ENC(ch+1, 1) = nan;
+    end
+    xlabel('Incoming Energy [keV]');
+    ylabel('Hit [\%]');
+    title("\textbf{Threshold Scan - Ch. " + num2str(ch) + "}");
+    exportgraphics(gcf, ['output/single_channels_THR_200/Scan di carica - ch ' num2str(ch) '.pdf'], 'ContentType', 'vector');
+end
+
+f = figure("Visible", "on");
+hold on
+plot([0:31], ENC)
+plot([0:31], THR)
+hold off
+
+data = [[0:31]', round(THR, 3), round(ENC, 3)];
+data_table = array2table(data, "VariableNames", ["Channel", "Threshold", "ENC"]);
+writetable(data_table, "ENC_THR_data_charge_scan_THR_200.dat", "Delimiter", "\t")
+
+
+%% SINGLE CHANNELS (THR = 214)
 
 importedData = readmatrix(['input/scan_carica_TH_214.dat']);
 

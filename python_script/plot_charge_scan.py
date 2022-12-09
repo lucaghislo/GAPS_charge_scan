2c8448a60af7c1699d7ac5ea7d6285e640e66d91
Luca Ghislotti <ghislottiluca@gmail.com>
2022-12-09 13:01:54 -0800

    Create plot_charge_scan.py

---
+++ python_script/plot_charge_scan.py
@@ -0,0 +1,10 @@
+import os as os
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv(
+    "input\SSL_Berkeley\FTH\IT_L4R0M0_Gigi_charge_scan_THR_205_FTH_MX.dat",
+    sep=" ",
+)
+
+print(df)

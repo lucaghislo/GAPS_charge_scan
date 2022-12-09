e6d311917447f6271e550fab0959ed962e3d52fc
Luca Ghislotti <ghislottiluca@gmail.com>
2022-12-09 09:36:55 -0800

    Added charge scans

--- python_script/compute_ERF.py
+++ python_script/compute_ERF.py
@@ -7,7 +7,14 @@ from erf_function import *
 import scipy as sp
 from scipy.stats import norm
 
-filename = "IT_L4R0M5_Gigi_m27C_charge_scan_THR_205_noFTH.dat"
+filename_0 = "IT_L4R0M0_Gigi_m27.2C_charge_scan_THR_205_noFTH.dat"
+filename_1 = "IT_L4R0M1_Gigi_m30.4C_charge_scan_THR_205_noFTH.dat"
+filename_2 = "IT_L4R0M2_Gigi_m29.6C_charge_scan_THR_205_noFTH.dat"
+filename_3 = "IT_L4R0M3_Gigi_m29.6C_charge_scan_THR_205_noFTH.dat"
+filename_4 = "IT_L4R0M4_Gigi_m29.7C_charge_scan_THR_205_noFTH.dat"
+filename_5 = "IT_L4R0M5_Gigi_m27C_charge_scan_THR_205_noFTH.dat"
+
+filename = filename_5
 filepath = os.path.join("output\SSL_Berkeley\data", filename)
 
 # Open file in read mode
@@ -16,31 +23,44 @@ data = pd.read_csv(
     sep="\t",
 )
 
+# Get data length
 x = data.iloc[:, 0]
 data = data.iloc[:, 1:33]
 
 parameters = np.zeros([1, 2])
 
+# Analyze all channels
 for ch in range(0, 32):
 
+    # Get channel data
     ch_data = data.iloc[:, ch]
 
+    # Interpolate erf function
     popt, pcov = sp.optimize.curve_fit(erf_function, x, ch_data / 100)
 
+    # Plot data interpolation
     plt.plot(x, ch_data / 100)
     plt.plot(x, erf_function(x, *popt))
-    # plt.show()
 
+    # Get mean: threshold
     mu = popt[0]
+
+    # Get standard deviation: dispersion
     sigma = popt[1] * 2.35
 
     parameters = np.vstack([parameters, [mu, sigma]])
-
-    print([mu, sigma])
-
     print("channel " + str(ch) + " -> mu: " + str(mu) + "\tsigma: " + str(sigma))
 
 parameters = parameters[1:, :]
+
+# Write parameters to file
+with open(
+    os.path.join("output\SSL_Berkeley\erf_fit_results\data", filename + "_THR_ENC.dat"),
+    "w",
+) as filehandle:
+    for i in range(0, 32):
+        print("Parameter: " + str(parameters[i, 0]))
+        filehandle.write("%f %f\n" % (parameters[i, 0], parameters[i, 1]))
 
 # Plot histogram of threshold data
 plt.clf()
@@ -74,4 +94,7 @@ plt.title(
     "ENC from Charge Scan\n" + str(filename),
     fontweight="bold",
 )
-plt.savefig("output\SSL_Berkeley\erf_fit_results\\" + str(filename) + "_ENC.pdf")
+plt.savefig("output\SSL_Berkeley\erf_fit_results\\" + filename + "_ENC.pdf")
+
+
+# Plot threhsold values for all modules on row

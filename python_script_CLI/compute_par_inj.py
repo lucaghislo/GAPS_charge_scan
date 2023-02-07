cc48522cfeffcc8ede4aeb56287d7786ab7e3bfd
Luca Ghislotti <ghislottiluca@gmail.com>
2023-02-07 15:53:51 +0100

    Fully working parasitic injection compensation

--- python_script_CLI/compute_par_inj.py
+++ python_script_CLI/compute_par_inj.py
@@ -19,12 +19,24 @@ def linear_model(x, m, q):
 def get_parasitic_injection(pedestal_filepath, fdt_filepath, ch, pt):
     # Read channel pedestal given peaking time
     pedestal_ch = get_pedestal_auto(pedestal_filepath, ch)
-    pedestal_ch = pedestal_ch * 0.841
 
     # Calculate transfer function linear gain and estimate pedestal
     (fdt_gain, fdt_pedestal) = get_linear_gain_realfdt(fdt_filepath, ch, pt, 200)
 
     # Determine parasitic injection
     ch_par_inj = abs(pedestal_ch - fdt_pedestal)
+
+    print(
+        "Ch. "
+        + str(ch)
+        + "\tGain: "
+        + str(np.round(fdt_gain, 5))
+        + "\tPedestal fdt:"
+        + str(np.round(fdt_pedestal, 5))
+        + "\tPedestal auto: "
+        + str(np.round(pedestal_ch, 5))
+        + "\tPar. inj.: "
+        + str(np.round(ch_par_inj, 5))
+    )
 
     return ch_par_inj
--- python_script_CLI/read_pedestals.py
+++ python_script_CLI/read_pedestals.py
@@ -15,33 +15,8 @@ def get_pedestal_auto(filepath_ped, ch):
     raw_pedestals = pd.read_csv(filepath_ped, sep="\t", comment="#", header=None)
     raw_pedestals_ch = raw_pedestals[raw_pedestals.loc[:, 3] == ch]
     raw_pedestals_ch = raw_pedestals_ch.loc[:, 4].to_numpy()
+
     mean_ped = np.mean(raw_pedestals_ch)
-
-    # Extract mean pedestal from pedestal raw data
-    (counts, bins, patches) = plt.hist(
-        raw_pedestals_ch[
-            (raw_pedestals_ch >= mean_ped * 0.9) & (raw_pedestals_ch <= mean_ped * 1.1)
-        ],
-        bins=np.arange(mean_ped * 0.9, mean_ped * 1.1, 1),
-    )
-
-    bins = bins[1::]
-    x = bins
-    y = counts
-
-    params = [mean_ped, 1, 1]
-    fitted_params, _ = scipy.optimize.curve_fit(
-        single_norm, x, y, p0=params, maxfev=20000
-    )
-
-    ped_auto = fitted_params[0]
-
-    plt.clf()
-    plt.hist(
-        raw_pedestals_ch[
-            (raw_pedestals_ch >= mean_ped * 0.9) & (raw_pedestals_ch <= mean_ped * 1.1)
-        ],
-        bins=np.arange(mean_ped * 0.9, mean_ped * 1.1, 1),
-    )
+    ped_auto = mean_ped
 
     return ped_auto
